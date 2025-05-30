857fed49d87514e3a0468046aa751c36fa838219
Syna <smalhan2@asu.edu>
2025-05-30 14:13:49 +0530

    Update

--- run_all.sh
+++ run_all.sh
@@ -7,7 +7,7 @@ echo "Running generate-project.sh..."
 # Check if generate-project.sh succeeded
 if [ $? -eq 0 ]; then
   echo "generate-project.sh completed successfully, now running deploy.sh..."
-  ./deploy.sh
+  echo "New project" | ./deploy.sh
 else
   echo "generate-project.sh failed, aborting deploy.sh."
   exit 1
